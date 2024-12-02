dd3a82fb79256449ccf57bd809e8379b134758ad
Artem Kireev <kireev165@gmail.com>
2024-12-02 14:15:06 +0300

    configure swift-openapi-generator to generate code for Github API

--- GithubAPI/Package.swift
+++ GithubAPI/Package.swift
@@ -5,17 +5,34 @@ import PackageDescription
 
 let package = Package(
     name: "GithubAPI",
+    platforms: [
+        .macOS(.v15),
+        .iOS(.v18),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GithubAPI",
             targets: ["GithubAPI"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-openapi-generator", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-runtime", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-openapi-urlsession", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GithubAPI"),
+            name: "GithubAPI",
+            dependencies: [
+                .product(name: "OpenAPIRuntime", package: "swift-openapi-runtime"),
+                .product(name: "OpenAPIURLSession", package: "swift-openapi-urlsession"),
+            ],
+            plugins: [
+                .plugin(name: "OpenAPIGenerator", package: "swift-openapi-generator"),
+            ]
+        ),
         .testTarget(
             name: "GithubAPITests",
             dependencies: ["GithubAPI"]
